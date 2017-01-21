a70ffddc04329a47cbbba02b3ffdd8b908cf7b55
maxvanbel <maxvanbel@gmail.com>
2017-01-21 09:58:49 -0600

    fix small bugs

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,8 +1,11 @@
+// set dependency, which is the data in friends.js
 var friendsArray = require('../data/friends.js');
 module.exports = function (app) {
+	// Get the data from the friendsArray
     app.get('/api/friends', function (req, res) {
         res.json(friendsArray);
     });
+    // Post data to friendsArray
     app.post('/api/friends', function (req, res) {
     	friendsArray.push(req.body);
     	res.json(friendsArray);
