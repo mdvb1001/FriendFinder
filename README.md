8054b6cba2bc2f188e922f87a43a9fa88c0ef789
maxvanbel <maxvanbel@gmail.com>
2017-01-21 09:48:14 -0600

    final stuff

--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 
 https://blooming-dusk-12678.herokuapp.com
 
-This is app
+This app matches the user with most compatible friend.
+
+The user will answer 10 personality questions. These answers are then compared to the answers of all the other 'friends' in local database.
+The person who has the closest results will be matched to the user.
+
+
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,16 +1,18 @@
 var path = require('path');
 module.exports = function (app) {
     // Routes
-    // =============================================================
+ 		// To survey page
     app.get("/survey", function (req, res) {
         res.sendFile(path.join(__dirname, "/../public/survey.html"));
     });
+    // To list of friends in json format
     app.get("/api/friends", function (req, res) {
         res.sendFile(path.join(__dirname, "/../data/friends.js"));
     });
+    // For home page, user can input anything that isn't the above
     app.use(function (req, res) {
         res.sendFile(path.join(__dirname, "/../public/home.html"));
-    });   
+    });
 
 
 };
